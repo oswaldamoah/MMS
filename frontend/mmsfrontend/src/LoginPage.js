297e49abdee2def3ea2f17d0a39dd7175ef31f1c
Oswald <103337835+oswaldamoah@users.noreply.github.com>
2024-07-30 04:00:42 +0000

    Update LoginPage.js

--- frontend/mmsfrontend/src/LoginPage.js
+++ frontend/mmsfrontend/src/LoginPage.js
@@ -1,40 +1,37 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import './LoginPage.css';
-import './admin_Pages/adminLogs'
 
 function LoginPage() {
   const [isSignUp, setIsSignUp] = useState(false);
   const navigate = useNavigate();
 
- // In LoginPage.js
-const handleLoginSubmit = async (event) => {
-  event.preventDefault();
-  const username = event.target.username.value;
-  const password = event.target.password.value;
+  const handleLoginSubmit = async (event) => {
+    event.preventDefault();
+    const username = event.target.username.value;
+    const password = event.target.password.value;
 
-  try {
-    const response = await fetch('https://mms-0tpv.onrender.com/api/login', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ username, password }),
-    });
+    try {
+      const response = await fetch('http://localhost:5000/api/login', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ username, password }),
+      });
 
-    if (response.ok) {
-      // Store the username in localStorage upon successful login
-      localStorage.setItem('username', username);
-      navigate('/Admin_Logs');
-    } else {
-      const data = await response.json();
-      alert(data.error || 'Login failed');
+      if (response.ok) {
+        localStorage.setItem('username', username);
+        navigate('/members');
+      } else {
+        const data = await response.json();
+        alert(data.error || 'Login failed');
+      }
+    } catch (error) {
+      console.error('Error logging in:', error);
+      alert('Login failed');
     }
-  } catch (error) {
-    console.error('Error logging in:', error);
-    alert('Login failed');
-  }
-};
+  };
 
   const handleSignUpSubmit = async (event) => {
     event.preventDefault();
@@ -42,25 +39,24 @@ const handleLoginSubmit = async (event) => {
     const password = event.target.password.value;
     const confirmPassword = event.target['confirm-password'].value;
     const passphrase = event.target.passphrase.value;
-  
+
     if (password !== confirmPassword) {
       alert('Passwords do not match');
       return;
     }
-  
+
     try {
-      const response = await fetch('https://mms-0tpv.onrender.com/api/signup', {
+      const response = await fetch('http://localhost:5000/api/signup', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
         },
         body: JSON.stringify({ username, password, passphrase }),
       });
-  
+
       if (response.ok) {
-        // Handle successful signup
         alert('Signup successful');
-        setIsSignUp(false); // Redirect to login after successful signup
+        setIsSignUp(false);
       } else {
         const data = await response.json();
         alert(data.error || 'Signup failed');
@@ -70,7 +66,6 @@ const handleLoginSubmit = async (event) => {
       alert('Signup failed');
     }
   };
-  
 
   const handleSignUpClick = () => {
     setIsSignUp(true);
@@ -81,14 +76,14 @@ const handleLoginSubmit = async (event) => {
   };
 
   return (
-    <div className="container">
+    <div className="container-login">
       {!isSignUp && (
-         <div className="welcome-section">
-        <img src="/logo1.png" alt="FFIM Logo" className="logo" />
-        <h1>WELCOME BACK!</h1>
-        <p>To stay connected with us, please login with your info.</p>
-        <p>Thank You.</p>
-      </div>
+        <div className="welcome-section-login">
+          <img src="/logo1.png" alt="FFIM Logo" className="logo-login" />
+          <h1>WELCOME BACK!</h1>
+          <p>To stay connected with us, please login with your info.</p>
+          <p>Thank You.</p>
+        </div>
       )}
       {isSignUp ? (
         <SignUpSection handleSignUpSubmit={handleSignUpSubmit} handleLoginClick={handleLoginClick} />
@@ -101,20 +96,20 @@ const handleLoginSubmit = async (event) => {
 
 function LoginSection({ handleSubmit, handleSignUpClick }) {
   return (
-    <div className="login-section">
-      <form className="form-control" onSubmit={handleSubmit}>
-        <p className="title">Login</p>
-        <div className="input-field">
-          <input required className="input" type="text" name="username" />
-          <label className="label" htmlFor="username">Enter Username</label>
+    <div className="form-control-login">
+      <form onSubmit={handleSubmit}>
+        <p className="title-login">Login</p>
+        <div className="input-field-login">
+          <input required className="input-login" type="text" name="username" />
+          <label className="label-login" htmlFor="username">Enter Username</label>
         </div>
-        <div className="input-field">
-          <input required className="input" type="password" name="password" />
-          <label className="label" htmlFor="password">Enter Password</label>
+        <div className="input-field-login">
+          <input required className="input-login" type="password" name="password" />
+          <label className="label-login" htmlFor="password">Enter Password</label>
         </div>
-        <button className="submit-btn" type="submit">Sign In</button>
-        <p className="signuptext">ARE YOU NEW HERE?</p>
-        <button type="button" className="signup" onClick={handleSignUpClick}>SIGN UP!</button>
+        <button className="submit-btn-login" type="submit">Sign In</button>
+        <p className="signuptext-login">ARE YOU NEW HERE?</p>
+        <button type="button" className="signup-login" onClick={handleSignUpClick}>SIGN UP!</button>
       </form>
     </div>
   );
@@ -122,35 +117,33 @@ function LoginSection({ handleSubmit, handleSignUpClick }) {
 
 function SignUpSection({ handleSignUpSubmit, handleLoginClick }) {
   return (
-    <div className="form-box">
-      <form className="form" onSubmit={handleSignUpSubmit}>
-        <p className="title">Sign up</p>
-        <span className="subtitle">Create a free account.</span>
-        <div className="input-field">
-          <input required className="input" type="text" name="username" placeholder="" />
-          <label className="label" htmlFor="username">Username</label>
+    <div className="form-box-login">
+      <form onSubmit={handleSignUpSubmit}>
+        <p className="title-login">Sign up</p>
+        <span className="signuptext-login">Create a free account.</span>
+        <div className="input-field-login">
+          <input required className="input-login" type="text" name="username" />
+          <label className="label-login" htmlFor="username">Username</label>
         </div>
-        <div className="input-field">
-          <input required className="input" type="password" name="password" placeholder="" />
-          <label className="label" htmlFor="password">Password</label>
+        <div className="input-field-login">
+          <input required className="input-login" type="password" name="password" />
+          <label className="label-login" htmlFor="password">Password</label>
         </div>
-        <div className="input-field">
-          <input required className="input" type="password" name="confirm-password" placeholder="" />
-          <label className="label" htmlFor="confirm-password">Confirm Password</label>
+        <div className="input-field-login">
+          <input required className="input-login" type="password" name="confirm-password" />
+          <label className="label-login" htmlFor="confirm-password">Confirm Password</label>
         </div>
-        <div className="input-field">
-          <input required className="input" type="text" name="passphrase" placeholder="" />
-          <label className="label" htmlFor="passphrase">Special Key</label>
+        <div className="input-field-login">
+          <input required className="input-login" type="text" name="passphrase" />
+          <label className="label-login" htmlFor="passphrase">Special Key</label>
         </div>
-        <button className="submit-btn2" type="submit">Sign up</button>
+        <button className="submit-btn2-login" type="submit">Sign up</button>
       </form>
-      <div className="form-section">
-        <p>Have an account? <button type="button" className="login-link" onClick={handleLoginClick}>Log in</button></p>
+      <div className="form-section-login">
+        <p>Have an account? <button type="button" className="login-link-login" onClick={handleLoginClick}>Log in</button></p>
       </div>
     </div>
   );
 }
-
-
 
 export default LoginPage;
