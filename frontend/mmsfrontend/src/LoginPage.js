0241928ce8dd9861a9fa5bb999248799baf3a490
Oswald <103337835+oswaldamoah@users.noreply.github.com>
2024-07-30 01:10:57 +0000

    Update LoginPage.js
    replacing endpoints with Render endpoints

--- frontend/mmsfrontend/src/LoginPage.js
+++ frontend/mmsfrontend/src/LoginPage.js
@@ -14,7 +14,7 @@ const handleLoginSubmit = async (event) => {
   const password = event.target.password.value;
 
   try {
-    const response = await fetch('http://localhost:5000/api/login', {
+    const response = await fetch('http://localhost:5000/api/login', {https://mms-0tpv.onrender.com
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
@@ -49,7 +49,7 @@ const handleLoginSubmit = async (event) => {
     }
   
     try {
-      const response = await fetch('http://localhost:5000/api/signup', {
+      const response = await fetch('https://mms-0tpv.onrender.com/api/signup', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
