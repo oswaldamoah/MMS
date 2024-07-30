a2963a75bcfa0e6c1cc530e1e6fef7b212201d2d
Oswald <103337835+oswaldamoah@users.noreply.github.com>
2024-07-30 01:12:12 +0000

    Update LoginPage.js

--- frontend/mmsfrontend/src/LoginPage.js
+++ frontend/mmsfrontend/src/LoginPage.js
@@ -14,7 +14,7 @@ const handleLoginSubmit = async (event) => {
   const password = event.target.password.value;
 
   try {
-    const response = await fetch('http://localhost:5000/api/login', {https://mms-0tpv.onrender.com
+    const response = await fetch('https://mms-0tpv.onrender.com/api/login', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
