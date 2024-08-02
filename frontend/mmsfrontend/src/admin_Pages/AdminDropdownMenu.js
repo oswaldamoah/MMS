281482c2b58659e73fd14b84554719a6f8671afb
Oswald <103337835+oswaldamoah@users.noreply.github.com>
2024-08-02 00:59:48 +0000

    Update AdminDropdownMenu.js

--- frontend/mmsfrontend/src/admin_Pages/AdminDropdownMenu.js
+++ frontend/mmsfrontend/src/admin_Pages/AdminDropdownMenu.js
@@ -10,10 +10,10 @@ function DropdownMenu({ isVisible, onOptionClick }) {
   return (
     <div className="dropdown-menu">
       <ul>
-        <li onClick={() => onOptionClick('/members')}>Members</li>
+        <li onClick={() => onOptionClick('/memberManagement')}>Members</li>
         <li onClick={() => onOptionClick('/editAnnouncements')}>Edit Announcements</li>
         <li onClick={() => onOptionClick('/editEvents')}>Edit Events</li>
-        <li onClick={() => onOptionClick('/editPaymentInfo')}>Edit Payment Info</li>
+        <li onClick={() => onOptionClick('//editPaymentOptions')}>Edit Payment Info</li>
         <li onClick={() => onOptionClick('/adminLogs')}>Admin Activity</li>
         <li onClick={() => onOptionClick('/Login')}>LOGOUT</li>
       </ul>
