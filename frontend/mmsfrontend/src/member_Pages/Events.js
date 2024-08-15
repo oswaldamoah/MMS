b1c912f41bfbbbcf300474827b601ae8bb272a71
oswaldamoah <oswaldamoah00@gmail.com>
2024-08-15 17:09:05 +0000

    lazy load fix

--- frontend/mmsfrontend/src/member_Pages/Events.js
+++ frontend/mmsfrontend/src/member_Pages/Events.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import MemberHeader from './MemberHeader';
 import ImagePreview from './ImagePreview';
-import { LazyLoadImage } from 'react-lazy-load-image-component';
 import Loader from './Loader'; // Import the Loader component
 import './Events.css';
 
@@ -37,7 +36,7 @@ const Events = () => {
   return (
     <div className="events-page">
       <MemberHeader headertitle="Events" />
-      <br></br>
+      <br />
       <div className="events-container">
         {loading ? (
           <Loader LoaderMessage="Getting events ready..." /> // Show loader while fetching events
@@ -47,12 +46,11 @@ const Events = () => {
             <div className="events-grid">
               {events.map((event, index) => (
                 <div key={index} className="event-card">
-                  <LazyLoadImage
+                  <img
                     src={`http://localhost:5000/api/events/image/${event._id}`}
                     alt={event.eventName}
                     className="event-image"
                     onClick={() => handleImageClick(`http://localhost:5000/api/events/image/${event._id}`)}
-                    effect="opacity" // Use 'opacity' as a default effect
                   />
                   <h3>{event.eventName}</h3>
                   <p>{event.eventDescription}</p>
